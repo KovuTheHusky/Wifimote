50054288807fc65dfba3307bd01de7cd3785f08a
KovuTheHusky <kovuthehusky@gmail.com>
2019-03-28 00:46:19 -0400

    Remove build

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /Users/kovuthehusky/Projects/Wifimote/Wifimote/src/libArduino.a
-
-LIBS := -lm
-
